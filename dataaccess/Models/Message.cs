3337d9e2fd501d3dc3a8c71ca568187566049486
Marcus Kovacs <marcus.kovacs@vitec.se>
2018-06-27 12:45:43 +0200

    Update Model with MaxLength

--- dataaccess/Models/Message.cs
+++ dataaccess/Models/Message.cs
@@ -6,7 +6,9 @@ namespace Messages.DataAccess.Models
     {
         public long Id { get; set; }
         [Required]
+        [MaxLength(50)]
         public string Title { get; set; }
+        [MaxLength(500)]
         public string Body { get; set; }
     }
 }
